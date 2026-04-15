972f237748cda089e478b10da1c0996917b67a5c
mao-ymst <47tegu8yun@gmail.com>
2026-04-15 21:49:18 +0900

    WIP: Add Board return and calculateWinner

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,3 +1,7 @@
+
+import calculateWinner from "../utils/calculateWinner";
+import Square from "./Square";
+
 //3 どのマスが押され,なにを置くかを管理
 
 export default function Board({squares,xIsNext,onPlay}) {
@@ -22,12 +26,32 @@ export default function Board({squares,xIsNext,onPlay}) {
     //Xの番(true)ならX,違うならOと処理を分ける
     //マス目をクリックしたら　変化した盤の状態をgameに伝える
 
+
+    const winner = calculateWinner(squares);
+    let status;
+    if(winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Next Player: " + (xIsNext ? "X" : "O");
+    }
+
+    // 5 gameの勝敗を判定-->勝利：勝者を表示　続行中：次の手番を表示
+    // calculateWinner をimportする
+    // winnerで勝った(そろった)記号が返ってきた又は続行の指示なら　それぞれの内容を表示する
+
     return (
         <>
-            <div>Board</div>
-
+            <div>{status}</div>
+            <div>
+                <Square value={squares[0]} onSquareClick={() => handleClick(0)}></Square>
+                <Square value={squares[1]} onSquareClick={() => handleClick(1)}></Square>
+                <Square value={squares[2]} onSquareClick={() => handleClick(2)}></Square>
+            </div>
         </>
   )
-  //4 Squaresを9個並べる
 
 }
+
+//6　squaresを9つ並べたboardの表示を作る
+// status を反映させる 
+// squareにvalue　handleClickの処理を一つずつ行う
---
+++ src/utils/calculateWinner.js
@@ -0,0 +1,31 @@
+//4 winnerを判定する
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for ( let i = 0; i < lines.length; i++) {
+
+        const [a, b, c] = lines[i];
+
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+
+    return null;
+}
+
+export default calculateWinner;
+
+// 勝負が決まる並びを設定する
+//８通りのうちどれかに当てはまっているか確認する (a,b,cがどれかなら)
+//squares[a]　aのマスが空でなく、b,cにaと同じ記号が並んでいるかを確認(つまりb,cも空ではない)
+//そろっている記号を返す そろっていなければ続行
