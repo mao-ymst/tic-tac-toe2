39446323d8f6663a85c58fa56521b5eacd06d7b5
mao-ymst <47tegu8yun@gmail.com>
2026-04-17 23:27:43 +0900

    Add: Board and Square CSS

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -41,18 +41,18 @@ export default function Board({ squares, xIsNext, onPlay }) {
 
     return (
         <>
-            <div>{status}</div>
-            <div>
+            <div className="status">{status}</div>
+            <div className="board-row">
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)}></Square>
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)}></Square>
                 <Square value={squares[2]} onSquareClick={() => handleClick(2)}></Square>
             </div>
-            <div>
+            <div className="board-row">
                 <Square value={squares[3]} onSquareClick={() => handleClick(3)}></Square>
                 <Square value={squares[4]} onSquareClick={() => handleClick(4)}></Square>
                 <Square value={squares[5]} onSquareClick={() => handleClick(5)}></Square>
             </div>
-            <div>
+            <div className="board-row">
                 <Square value={squares[6]} onSquareClick={() => handleClick(6)}></Square>
                 <Square value={squares[7]} onSquareClick={() => handleClick(7)}></Square>
                 <Square value={squares[8]} onSquareClick={() => handleClick(8)}></Square>
